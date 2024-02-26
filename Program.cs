3e6de93297470db8fb859e826ea3ce1ceb59144f
JOSE ANGEL LUGO ESPARZA <lugo.joseangel.c66@gmail.com>
2024-02-26 15:29:43 -0600

    MesaCore Context

--- Program.cs
+++ Program.cs
@@ -10,7 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 
-builder.Services.AddDbContext<MesaCoreContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddDbContext<MesaCoreContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 //Services
